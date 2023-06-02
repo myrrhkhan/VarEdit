b3ae337cf997beb8f4fa6bbd5aee8b0fd4793b54
Myrrh Khan <myrrhkhan@gmail.com>
2023-06-02 15:36:13 -0400

    GOT BASIC FUNCTIONALITY TO WORK

--- src/lib/Vars.svelte
+++ src/lib/Vars.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import { invoke } from "@tauri-apps/api/tauri";
-	
-	async function getPath(): Promise<Map<string, string[]>> {
-		let thing: Map<string, string[]> = await invoke('get_vars');
+
+	interface variableMap {
+		[key: string]: string[];
+	}
+
+	async function getPath(): Promise<variableMap> {
+		let thing: variableMap = await invoke('get_vars');
 		return thing;
 	}
 	
@@ -10,13 +14,14 @@
 
 </script>
 
-<h1>Edit Environment Variables</h1>
+<h1>Your Computer's Environment Variables:</h1>
 
 {#await varsPromise then allVars}
-	{#each [...allVars] as [key, vals]}
-		<h3>Values for {key}</h3>
-		{#each vals as val}
-			<li>{val}</li>
+	{#each Object.keys(allVars) as key}
+		{@const values = allVars[key]}
+		<h3>{key}</h3>
+		{#each values as value}
+			<li>{value}</li>
 		{/each}
 	{/each}
 {/await}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <script>
 	import Vars from '$lib/Vars.svelte';
-	import Greet from '../lib/Greet.svelte'
 </script>
 
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<!-- <Greet /> -->
 <Vars />
