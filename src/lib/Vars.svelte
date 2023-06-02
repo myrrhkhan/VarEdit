ca91a8fc38704484788f650144d3249db323862b
Myrrh Khan <myrrhkhan@gmail.com>
2023-06-02 11:30:28 -0400

    figured out easier way to iterate through map but it's still not working lol

--- src/lib/Vars.svelte
+++ src/lib/Vars.svelte
@@ -3,43 +3,20 @@
 	
 	async function getPath(): Promise<Map<string, string[]>> {
 		let thing: Map<string, string[]> = await invoke('get_vars');
-		console.log(thing);
 		return thing;
 	}
 	
 	let varsPromise = getPath();
-	
-	function handleClick() {
-		varsPromise = getPath();
-	}
-
-	/**
-	 * 
-	 * @param map
-	 * @param key
-	 */
-	async function getKeys(map: Map<string, string[]>, key: string): Promise<string[]> {
-		let thing: string[] = (map!).get(key)!;
-		return thing;
-	}
-	
 
 </script>
 
 <h1>Edit Environment Variables</h1>
 
-
 {#await varsPromise then allVars}
-	{#each (Object.keys(allVars)) as key}
-		<h3>{key}</h3>
-		{#if allVars}
-			<p>something exists</p>
-			{#await getKeys(allVars, key) then values}
-				console.log("successfully awaited");
-				{#each values as value}
-					<p>{value}</p>
-				{/each}
-			{/await}
-		{/if}
+	{#each [...allVars] as [key, vals]}
+		<h3>Values for {key}</h3>
+		{#each vals as val}
+			<li>{val}</li>
+		{/each}
 	{/each}
 {/await}
